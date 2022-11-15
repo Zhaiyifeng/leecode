1901b7e4a40a8faa3dd9cf96baadeef369763368
zhaiyifeng <1418265878@qq.com>
2022-11-15 21:33:19 +0800

    测试

--- src/main/java/com/example/leecode/LeecodeApplication.java
+++ src/main/java/com/example/leecode/LeecodeApplication.java
@@ -3,6 +3,9 @@ package com.example.leecode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ */
 @SpringBootApplication
 public class LeecodeApplication {
 
