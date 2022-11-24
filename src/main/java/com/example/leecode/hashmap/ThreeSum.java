f87af4f3895c84e9d1918c3d220f242a36d5e677
zhaiyifeng <1418265878@qq.com>
2022-11-25 02:08:51 +0800

    2022/11/25 15. 三数之和

--- src/main/java/com/example/leecode/hashmap/ThreeSum.java
+++ src/main/java/com/example/leecode/hashmap/ThreeSum.java
@@ -1,17 +1,90 @@
 package com.example.leecode.hashmap;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ThreeSum {
     public static void main(String[] args) {
         int[] ints = {-1, 0, 1, 2, -1, -4};
-        threeSum(ints);
+        System.out.println(threeSum(ints));
     }
 
+    /**
+     * 15. 三数之和
+     * <a href="https://leetcode.cn/problems/3sum/">...</a>
+     * 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请
+     * <p>
+     * 你返回所有和为 0 且不重复的三元组。
+     * <p>
+     * 注意：答案中不可以包含重复的三元组。
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：nums = [-1,0,1,2,-1,-4]
+     * 输出：[[-1,-1,2],[-1,0,1]]
+     * 解释：
+     * nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+     * nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+     * nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+     * 不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+     * 注意，输出的顺序和三元组的顺序并不重要。
+     * 示例 2：
+     * <p>
+     * 输入：nums = [0,1,1]
+     * 输出：[]
+     * 解释：唯一可能的三元组和不为 0 。
+     * 示例 3：
+     * <p>
+     * 输入：nums = [0,0,0]
+     * 输出：[[0,0,0]]
+     * 解释：唯一可能的三元组和为 0 。
+     * 提示：
+     * <p>
+     * 3 <= nums.length <= 3000
+     * -105 <= nums[i] <= 105
+     * Related Topics
+     * 数组
+     * 双指针
+     * 排序
+     * <p>
+     * 👍 5407
+     * 👎 0
+     */
     public static List<List<Integer>> threeSum(int[] nums) {
-        HashSet<int[]> set = new HashSet<>();
-        set.add(nums);
-        
+        //排序
+        Arrays.sort(nums);
+        List<List<Integer>> integers = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            //如果小于3或者最小的大于0那么就不用继续下去 因为不会成立
+            if (nums.length < 3 || nums[i] > 0) return integers;
+            //针对num[i]进行去重
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            //定义左右指针
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                //根据结果判断左右指针移动哪个
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum < 0) {
+                    left++;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    integers.add(Arrays.asList(nums[left], nums[right], nums[i]));
+                    //去重
+                    while (right > left && nums[left] == nums[left + 1]) left++;
+                    //去重
+                    while (right > left && nums[right] == nums[right - 1]) right--;
+                    //指针改变位置
+                    left++;
+                    //指针改变位置
+                    right--;
+                }
+
+            }
+        }
+        return integers;
     }
 }
